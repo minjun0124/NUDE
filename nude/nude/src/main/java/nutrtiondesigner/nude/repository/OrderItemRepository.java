1f4a25e2b58af55a15704144588238ad2ae8c46f
mjlee <minjun0124@gmail.com>
2021-07-27 00:37:48 +0900

    fix: OrderItemRepository - findFetchJoinByOrderCode separate count query

--- nude/nude/src/main/java/nutrtiondesigner/nude/repository/OrderItemRepository.java
+++ nude/nude/src/main/java/nutrtiondesigner/nude/repository/OrderItemRepository.java
@@ -1,17 +1,14 @@
 package nutrtiondesigner.nude.repository;
 
 import nutrtiondesigner.nude.model.domain.OrderItem;
-import nutrtiondesigner.nude.model.domain.Orders;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-    @Query("select oi from OrderItem oi join fetch Item where oi.orders.code = :orderItemCode")
+    @Query(value = "select oi from OrderItem oi join fetch oi.item where oi.orders.code = :orderItemCode"
+    , countQuery = "select oi from OrderItem oi where oi.orders.code = :orderItemCode")
     Page<OrderItem> findFetchJoinByOrderCode(@Param("orderItemCode") Long orderItemCode, Pageable pageable);
 }
